13e3e797156da07fac7187e61ba1569429a671eb
hananesi2 <hananeett1@gmail.com>
2023-03-12 10:53:38 +0100

    fix_errors

--- calculator.py
+++ calculator.py
@@ -1,14 +1,25 @@
-def add(a, b):
-    '''hgyun'''
-    return a + b
+"""
+This module provides basic arithmetic functions for addition, subtraction,
+multiplication, and division.
+"""
 
-def subtract(a, b):
-    return a - b
+def add(num_1, num_2):
+    """Return the sum of two numbers."""
+    return num_1 + num_2
 
-def multiply(a, b):
-    return a * b
+def subtract(num_1, num_2):
+    """Return the difference between two numbers."""
+    return num_1 - num_2
 
-def divide(a, b):
-    if b == 0:
-        raise ValueError(" we Cannot divide by ZERO!!!")
-    return a / b
+def multiply(num_1, num_2):
+    """Return the product of two numbers."""
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    """
+    Return the division of two numbers.
+    Raises ValueError if b is 0.
+    """
+    if num_2 == 0:
+        raise ValueError("Cannot divide by zero!")
+    return num_1 / num_2
--- test_calculator.py
+++ test_calculator.py
@@ -1,24 +1,31 @@
+"""Unit tests for calculator.py"""
+
 import unittest
 import calculator
 
 class TestCalculator(unittest.TestCase):
+    """Tests for the calculator functions"""
 
     def test_add(self):
+        """Test the add function"""
         self.assertEqual(calculator.add(2, 3), 5)
         self.assertEqual(calculator.add(-1, 1), 0)
         self.assertEqual(calculator.add(-1, -1), -2)
 
     def test_subtract(self):
+        """Test the subtract function"""
         self.assertEqual(calculator.subtract(2, 3), -1)
         self.assertEqual(calculator.subtract(-1, 1), -2)
         self.assertEqual(calculator.subtract(-1, -1), 0)
 
     def test_multiply(self):
+        """Test the multiply function"""
         self.assertEqual(calculator.multiply(2, 3), 6)
         self.assertEqual(calculator.multiply(-1, 1), -1)
         self.assertEqual(calculator.multiply(-1, -1), 1)
 
     def test_divide(self):
+        """Test the divide function"""
         self.assertEqual(calculator.divide(6, 3), 2)
         self.assertEqual(calculator.divide(-1, 1), -1)
         self.assertEqual(calculator.divide(-6, -3), 2)
